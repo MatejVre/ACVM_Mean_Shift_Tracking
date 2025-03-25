1c18dced2ff6236453dc7d8eac65b1fc12633686
Matej Vrecar <matej@vrecar.si>
2025-03-25 18:17:49 +0100

    Base solution works.

--- ms_tracker.py
+++ ms_tracker.py
@@ -12,37 +12,57 @@ class MeanShiftTracker():
         self.y = None
 
     def initialize(self, image, region):
-        patch_w = region[2] if region[2]%2 == 1 else region[2] + 1
-        patch_h = region[3] if region[3]%2 == 1 else region[3] + 1
-        x = region[0]
-        y = region[1]
-        patch = get_patch(image, (x, y), (int(patch_w), int(patch_h)))[0]
-        self.kernel = create_epanechnik_kernel(patch_w, patch_h, 2)
+        if len(region) == 8:
+            x_ = np.array(region[::2])
+            y_ = np.array(region[1::2])
+            region = [np.min(x_), np.min(y_), np.max(x_) - np.min(x_) + 1, np.max(y_) - np.min(y_) + 1]
+
+        x = max(region[0], 0)
+        y = max(region[1], 0)
+
+        self.position = (region[0] + region[2] / 2, region[1] + region[3] / 2)
+
+        right = min(region[0] + region[2], image.shape[1] - 1)
+        bottom = min(region[1] + region[3], image.shape[0] - 1)
+        patch_w = int(right - x)
+        patch_h = int(bottom - y)
+        patch_w = patch_w if patch_w %2 == 1 else patch_w + 1
+        patch_h = patch_h if patch_h %2 == 1 else patch_h + 1
+        patch = get_patch(image, (self.position), (int(patch_w), int(patch_h)))[0]
+        self.kernel = create_epanechnik_kernel(int(patch_w), int(patch_h), 0.8)
         hist_q = extract_histogram(patch, 16, self.kernel)
         self.hist_q = hist_q / np.sum(hist_q)
-        self.patch_w = patch_w
-        self.patch_h = patch_h
-        self.x = int(x)
-        self.y = int(y)
+        self.patch_w = int(patch_w)
+        self.patch_h = int(patch_h)
+        self.x = self.position[0]
+        self.y = self.position[1]
 
 
     def track(self, image):
-        dx = 10
-        dy = 10
-        i=1
-        while (dx != 0 or dy != 0) and (i < 20):
-            patch2 = get_patch(image, (self.x, self.y), (self.patch_w, self.patch_h))[0]
+        i=0
+        while (i < 20):
+            patch2, mask_kernel = get_patch(image, (round(self.x), round(self.y)), (self.patch_w, self.patch_h))
+            cur_ker = self.kernel * mask_kernel
             hist_p = extract_histogram(patch2, 16, self.kernel)
             hist_p_normalized = hist_p / np.sum(hist_p)
-            v = np.sqrt(self.hist_q/(hist_p_normalized + 1e-7))
+            v = np.sqrt(self.hist_q/(hist_p_normalized + 1e-5))
             back_p = backproject_histogram(patch2, v, 16)
-            x = back_p.shape[1]//2
-            y = back_p.shape[0]//2
-            new_x, new_y = mean_shift(back_p, 15, x_start=x, y_start=y, N_iterations=1)
-            self.x += (new_x - x)  
-            self.y += (new_y - y) 
+            x_range = np.arange(-(self.patch_w // 2), (self.patch_w // 2) + 1)
+            y_range = np.arange(-(self.patch_h // 2), (self.patch_h // 2) + 1)
+            
+            x_diffs, y_diffs = np.meshgrid(x_range, y_range)
+
+            dx = np.sum(x_diffs * back_p) / (np.sum(back_p) + 1e-7)
+            dy = np.sum(y_diffs * back_p) / (np.sum(back_p) + 1e-7)
+
+            if (np.sqrt(dx**2+dy**2) <= 1):
+                return(round(self.x - self.patch_w /2), round(self.y - self.patch_h/2), self.patch_w, self.patch_h)
+
+            self.x = ((self.x + dx))
+            self.y = ((self.y + dy))
             i += 1
-        return(self.x, self.y, self.patch_w, self.patch_h)
+
+        return(round(self.x - self.patch_w /2), round(self.y - self.patch_h/2), self.patch_w, self.patch_h)
 
 def mean_shift3(fun, kernel, x_start=None, y_start=None, N_iterations = 1):
     height, width = kernel.shape
@@ -62,12 +82,10 @@ def mean_shift3(fun, kernel, x_start=None, y_start=None, N_iterations = 1):
         x_diffs = (x_diffs / (width)) ** 2 # Normalize x distances
         y_diffs = (y_diffs / (height)) ** 2 # Normalize y distances
 
-        x_range = np.arange(width)
-        y_range = np.arange(height)
-        x_coords, y_coords = np.meshgrid(x_range, y_range)
+        #x_coords, y_coords = np.meshgrid(np.arange(width), np.arange(height))
 
-        x_new = int(x + np.sum(x_diffs * fun) / np.sum(fun))
-        y_new = int(y + np.sum(y_diffs * fun) / np.sum(fun))
+        x_new = x + int(np.sum(x_diffs * fun) / np.sum(fun))
+        y_new = y + int(np.sum(y_diffs * fun) / np.sum(fun))
         
         if ((x_new == x) and (y_new == y)) or N_iterations <= i:
             return x_new, y_new
